a53c7cb79b8ffe538bac5c08ca5aa82cdc9ca2c2
Jisus <100171248+zoociego@users.noreply.github.com>
2024-01-27 13:05:30 -0300

    hola2

---
+++ PigeonRumble/Assets/Scripts/IAPriority.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using UnityEngine;
+
+public class PriorityAI : MonoBehaviour
+{
+    private string currentState = "idle"; // Initial state
+    public wating = 4.5f
+    void Start()
+    { //hola
+        // empieza la priorizacion
+        StartCoroutine(Prioritize());
+    }
+
+    IEnumerator Prioritize()
+    {
+        while (true) // Comprobacion del estado del 
+        {
+            // De manera dinamica una vez que recibimos el estado los asignamos y comenzamos la logica
+            switch (currentState)
+            {
+                case "attacking":
+                    Debug.Log("Priority: Attacking");
+                    yield return new WaitForSeconds(wating); 
+                    break;
+                case "eating":
+                    Debug.Log("Priority: Eating");
+                    yield return new WaitForSeconds(wating);
+                    break;
+                case "idle":
+                default:
+                    Debug.Log("Priority: Idle");
+                    yield return new WaitForSeconds(wating); 
+                    break;
+            }
+        }
+    }
+
+    public void ChangeState(string newState)
+    {
+        currentState = newState;
+    }
+}
