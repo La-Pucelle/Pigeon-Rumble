adb1fc9d8b6657b360477bee4d3b7c26fcb9ea38
La-Pucelle <108086683+La-Pucelle@users.noreply.github.com>
2024-01-27 18:11:50 -0300

    .

--- PigeonRumble/Assets/Scripts/AI/Priority.cs
+++ PigeonRumble/Assets/Scripts/AI/Priority.cs
@@ -1,34 +1,56 @@
 using System.Collections;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class PriorityAI : MonoBehaviour
 {
-    public string currentState; // Initial state
-    public float wating = 4.5f;
+    public Target target;
+    public Transform Player;
+    public Transform Food;
+    string currentState = "idle"; // Initial state
+    public float wating = 0.5f;
     void Start()
     { //hola
         // empieza la priorizacion
         StartCoroutine(Prioritize());
     }
 
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            currentState = "attacking";
+        } 
+
+        else if (other.CompareTag("Food"))
+        {
+            currentState = "eating";
+        }
+
+    }
+
     IEnumerator Prioritize()
     {
         while (true) // Comprobacion del estado del 
         {
-            // De manera dinamica una vez que recibimos el estado los asignamos y comenzamos la logica
             switch (currentState)
             {
                 case "attacking":
                     Debug.Log("Priority: Attacking");
-                    yield return new WaitForSeconds(wating); 
+                    target.playerTransform = Player;
+                    yield return new WaitForSeconds(wating);
+                    currentState = "idle";
                     break;
                 case "eating":
                     Debug.Log("Priority: Eating");
+                    target.playerTransform = Food;
                     yield return new WaitForSeconds(wating);
+                    currentState = "idle";
                     break;
                 case "idle":
                 default:
                     Debug.Log("Priority: Idle");
+                    target.playerTransform = null;
                     yield return new WaitForSeconds(wating); 
                     break;
             }
